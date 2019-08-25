f028c1485b64612af665120e6506c6494012a182
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-25 12:18:10 +0700

    Add files via upload

---
+++ scriptbagus.sh
@@ -0,0 +1,489 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo
+echo
+echo
+echo
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+figlet -f smshadow "Mr"
+figlet -f smshadow "HSSH53"
+echo $white"_________________________________________"
+sleep 1
+echo $white"[1]"$green"HACK WA"
+echo $white"________________________________________"
+sleep 1
+echo $white"[2]"$green"HACK FB TARGET WORK"
+echo $white"________________________________________"
+sleep 1
+echo $white"[3]"$green"HACK INSTAGRAM"
+echo $white"________________________________________"
+echo $white"[4]"$green"HACK CCTV"
+echo $white"________________________________________"
+echo $white"[5]"$green"HACK PULSA ALL OPERATOR"
+echo $white"________________________________________"
+sleep 1
+echo $white"[6]"$green"HACK HP TEMAN"
+echo $white"________________________________________"
+echo "\033[34;1m  99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m PILIH DENGAN BIJAK!\033[32;1m"
+read -p " ANONYMOUS$ " pilih
+if [ $pilih = 1 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+echo "\033[34;1mLoading.....!!!"
+echo
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/termux
+ls
+cd termux
+sh termux.sh
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+echo
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/termux
+cd termux
+sh termux.sh
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+echo
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/termux
+cd termux
+sh termux.sh
+fi
+
+if [ $pilih = 4 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+echo
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/termux
+cd termux
+sh termux.sh
+fi
+
+if [ $pilih = 5 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+echo
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/termux
+cd termux
+sh termux.sh
+fi
+
+if [ $pilih = 6 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+echo
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/termux
+cd termux
+sh termux.sh
+fi
+
+if
